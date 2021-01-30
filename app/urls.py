eed8d7c030cacecc1d3c0d8f83a9cdf6898c9344
Ruslan <ruslaneleusinov@gmail.com>
2021-01-30 15:52:16 +0500

    Handle about-page and overall small changes

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomeView, ProjectsView
+from .views import HomeView, ProjectsView, AboutView
 
 app_name = 'app'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('about', AboutView.as_view(), name='about'),
     path('projects', ProjectsView.as_view(), name='projects'),
-    path('projects', ProjectsView.as_view(), name='contacts'),
+    path('contacts', ProjectsView.as_view(), name='contacts'),
 ]
